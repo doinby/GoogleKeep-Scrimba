a49b0fb8965537c553879b490089cf26ea33a9ba
Chinh Do <do.inby@gmail.com>
2021-08-25 17:51:07 +1000

    added submit note behavior, open/close form behavior

---
+++ app.js
@@ -0,0 +1,80 @@
+class App {
+    constructor() {
+        this.notes = [];
+        
+        this.$form = document.querySelector("#form");
+        this.$noteTitle = document.querySelector('#note-title');
+        this.$noteText = document.querySelector("#note-text");
+        this.$formButtons = document.querySelector('#form-buttons');
+        
+        this.addEventListeners();
+    }
+    
+    addEventListeners() {
+        document.body.addEventListener("click", (event) => {
+            this.handleFormClick(event);
+        });
+        
+        this.$form.addEventListener('submit', event => {
+            // Prevent JS's default refresh behavior when clicking submit
+            event.preventDefault();
+            const title = this.$noteTitle.value;
+            const text = this.$noteText.value;
+            
+            // Detect truthy values and them store in variable
+            // without having to write if/else
+            const hasNote = title || text;
+            
+            if(hasNote) {
+                // Instead of passing in each variable, we could pass in
+                // an object that includes those variable since we don't
+                // care about the order
+                this.addNote({title, text});
+            }
+        })
+    }
+    
+    handleFormClick(event) {
+        const isFormClicked = this.$form.contains(event.target); // .contain() returns a true or false value
+        
+        if(isFormClicked) {
+            this.openForm();
+        } else {
+            this.closeForm();
+        }
+        
+    }
+    
+    openForm() {
+        this.$form.classList.add('form-open');
+        this.$noteTitle.style.display = "block";
+        this.$formButtons.style.display = "block";
+    }
+    
+    closeForm() {
+        this.$form.classList.remove("form-open");
+        this.$noteTitle.style.display = "none";
+        this.$formButtons.style.display = "none";
+    }
+    
+    addNote(note) {
+        const newNote = {
+            title: note.title,
+            text: note.text,
+            color: 'white',
+            id: () => {
+                const idNumber = 1;
+                if (this.notes.length > 0) {
+                    // Find the last item in the notes array and add 1
+                    // to create next note's ID
+                    idNumber = this.notes[this.notes.length - 1].id + 1;
+                } else idNumber = 1; // If no note yet, set note's ID to 1
+                return idNumber;
+            }
+        }
+        this.notes = [...this.notes, newNote];
+        console.log(this.notes);
+    }
+}
+
+new App();
